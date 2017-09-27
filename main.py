56477f188b55c05896a962a4e906b060a0112ff3
Ziga Patacko Koderman <ziga.patacko@protonmail.com>
2017-09-27 10:56:30 +0200

    add README and main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+def load_data(data_dir):
+    # Get all subdirectories of data_dir. Each represents a label.
+    directories = [d for d in os.listdir(data_dir)
+                   if os.path.isdir(os.path.join(data_dir, d))]
+    # Loop through the label directories and collect the data in
+    # two lists, labels and images.
+    labels = []
+    images = []
+    for d in directories:
+        label_dir = os.path.join(data_dir, d)
+        file_names = [os.path.join(label_dir, f)
+                      for f in os.listdir(label_dir)
+                      if f.endswith(".ppm")]
+        for f in file_names:
+            images.append(skimage.data.imread(f))
+            labels.append(int(d))
+    return images, labels
+
+images, labels = load_data(train_data_dir)
